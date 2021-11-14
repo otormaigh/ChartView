d5861e0de9dc75e781f6fa4993e68649eef1939a
Elliot Tormey <elliot.tormey@gmail.com>
2021-11-14 19:10:37 +0000

    make background and title optional views in PiChartView

--- Sources/SwiftUICharts/PieChart/PieChartView.swift
+++ Sources/SwiftUICharts/PieChart/PieChartView.swift
@@ -10,12 +10,13 @@ import SwiftUI
 
 public struct PieChartView : View {
     public var data: [PieChartData]
-    public var title: String
+    public var title: String?
     public var legend: String?
     public var style: ChartStyle
     public var formSize:CGSize
     public var dropShadow: Bool
     public var valueSpecifier:String
+    public var showBackground: Bool
     
     @State private var showValue = false
     @State private var currentValue: Double = 0 {
@@ -26,7 +27,7 @@ public struct PieChartView : View {
         }
     }
     
-    public init(data: [PieChartData], title: String, legend: String? = nil, style: ChartStyle = Styles.pieChartStyleOne, form: CGSize? = ChartForm.medium, dropShadow: Bool? = true, valueSpecifier: String? = "%.1f"){
+    public init(data: [PieChartData], title: String? = nil, legend: String? = nil, style: ChartStyle = Styles.pieChartStyleOne, form: CGSize? = ChartForm.medium, dropShadow: Bool? = true, valueSpecifier: String? = "%.1f", showBackground: Bool = false){
         self.data = data
         self.title = title
         self.legend = legend
@@ -37,31 +38,39 @@ public struct PieChartView : View {
         }
         self.dropShadow = dropShadow!
         self.valueSpecifier = valueSpecifier!
+        self.showBackground = showBackground
     }
     
     public var body: some View {
         ZStack{
-            Rectangle()
-                .fill(self.style.backgroundColor)
-                .cornerRadius(20)
-                .shadow(color: self.style.dropShadowColor, radius: self.dropShadow ? 12 : 0)
+            if (showBackground) {
+                Rectangle()
+                    .fill(self.style.backgroundColor)
+                    .cornerRadius(20)
+                    .shadow(color: self.style.dropShadowColor, radius: self.dropShadow ? 12 : 0)
+            }
+            
             VStack(alignment: .leading){
                 HStack{
-                    if(!showValue){
-                        Text(self.title)
+                    if(!showValue) {
+                        if (self.title != nil) {
+                        Text(self.title!)
                             .font(.headline)
                             .foregroundColor(self.style.textColor)
-                    }else{
+                        }
+                    } else {
                         Text("\(self.currentValue, specifier: self.valueSpecifier)")
                             .font(.headline)
                             .foregroundColor(self.style.textColor)
                     }
                     Spacer()
                 }.padding()
+                
                 PieChartRow(data: data, borderColor: self.style.accentColor, showValue: $showValue, currentValue: $currentValue)
                     .foregroundColor(self.style.accentColor)
                     .padding(self.legend != nil ? 0 : 12)
                     .offset(y:self.legend != nil ? 0 : -10)
+                
                 if(self.legend != nil) {
                     Text(self.legend!)
                         .font(.headline)
@@ -79,6 +88,10 @@ struct PieChartView_Previews : PreviewProvider {
     static var previews: some View {
         let data = [PieChartData(value: 25, color: Color.orange), PieChartData(value: 30, color: Color.blue), PieChartData(value: 45, color: Color.green)]
         PieChartView(data: data, title: "Title", legend: "Legend")
+        
+        PieChartView(data: data, showBackground: true)
+        
+        PieChartView(data: data)
     }
 }
 #endif
